c709b2f6c34ad20c5077e195b14b8e7dd90ff09c
Quintard Dylan <122774244+dylanquintard@users.noreply.github.com>
2024-02-23 12:04:15 +0100

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,52 +3,9 @@ import React, { Suspense, useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import Layout from "../components/Layout";
 import Sectiontitle from "../components/Sectiontitle";
-import Service from "../components/Service";
 import Spinner from "../components/Spinner";
 
 function About() {
-  const [toggler, setToggler] = useState(false);
-  const [information, setInformation] = useState("");
-  const [services, setServices] = useState([]);
-  const [reviews, setReviews] = useState([]);
-
-  const sliderSettings = {
-    dots: false,
-    infinite: true,
-    arrows: false,
-    speed: 500,
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    autoplay: true,
-    autoplaySpeed: 6000,
-    pauseOnHover: true,
-    adaptiveHeight: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
-  const handleToggler = (event) => {
-    setToggler(!toggler);
-  };
-
-  useEffect(() => {
-    axios.get("/api/information").then((response) => {
-      setInformation(response.data);
-    });
-    axios.get("/api/services").then((response) => {
-      setServices(response.data);
-    });
-    axios.get("/api/reviews").then((response) => {
-      setReviews(response.data);
-    });
-  }, []);
 
   return (
     <Layout>
@@ -70,11 +27,11 @@ function About() {
                     Je suis <span className="color-theme">Quintard Dylan</span>
                   </h3>
                   <p>
-                  Je suis développeur web full stack, spécialisé dans le développement d'application MERN. Mon expertise couvre la conception et le déploiement d'applications modernes, mettant l'accent sur des expériences utilisateur fluides et dynamiques. Situé proche de Toulouse, j'aime travailler sur de nouveaux projets et apprendre de nouvelles choses.  Ici vous trouverez toutes les informations concernant mes projets et mes compétences.
+                  Je suis développeur web et mobile fullstack JS, spécialisé dans React, Node.js (Express), et MongoDB. Mon expertise couvre la conception et le déploiement d'applications modernes, mettant l'accent sur des expériences utilisateur fluides et dynamiques. Situé proche de Toulouse, j'aime travailler sur de nouveaux projets et apprendre de nouvelles choses.  Ici vous trouverez toutes les informations concernant mes projets et mes compétences.
                   </p>
                   <ul>
                       <li>
-                        <b>Age</b> 27 ans
+                        <b>Age</b> 25 ans
                       </li>
                       <li>
                         <b>Téléphone</b> +33 7 49 34 70 12
@@ -96,23 +53,6 @@ function About() {
                     Télécharger mon CV
                   </a>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="mi-service-area mi-section mi-padding-top">
-          <div className="container">
-            <Sectiontitle title="Services" />
-            <div className="mi-service-wrapper">
-              <div className="row mt-30-reverse">
-                {services.map((service) => (
-                  <div
-                    className="col-lg-4 col-md-6 col-12 mt-30"
-                    key={service.title}
-                  >
-                    <Service content={service} />
-                  </div>
-                ))}
               </div>
             </div>
           </div>
