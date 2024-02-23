2884bb6570c142e206370ba49ff42c1a81580ffd
Quintard Dylan <122774244+dylanquintard@users.noreply.github.com>
2024-02-23 11:48:58 +0100

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -70,7 +70,7 @@ function About() {
                     Je suis <span className="color-theme">Quintard Dylan</span>
                   </h3>
                   <p>
-                  Je suis développeur web et mobile fullstack JS, spécialisé dans React, Node.js (Express), et MongoDB. Mon expertise couvre la conception et le déploiement d'applications modernes, mettant l'accent sur des expériences utilisateur fluides et dynamiques. Situé proche de Toulouse, j'aime travailler sur de nouveaux projets et apprendre de nouvelles choses.  Ici vous trouverez toutes les informations concernant mes projets et mes compétences.
+                  Je suis développeur web full stack, spécialisé dans le développement d'application MERN. Mon expertise couvre la conception et le déploiement d'applications modernes, mettant l'accent sur des expériences utilisateur fluides et dynamiques. Situé proche de Toulouse, j'aime travailler sur de nouveaux projets et apprendre de nouvelles choses.  Ici vous trouverez toutes les informations concernant mes projets et mes compétences.
                   </p>
                   <ul>
                       <li>
